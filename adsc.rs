72ca2888308d0ca4d1752561ff8bcd89cd40b2d5
川田 英幸 <hideyuki@MBA1112.local>
2017-11-12 16:35:08 +0900

    test

---
+++ adsc.rs
@@ -0,0 +1,21 @@
+use std::fs;
+use std::io::Write;
+
+extern crate rand;
+
+//use rand::Rng;
+
+fn main() {
+
+    let N = 50;
+    let orderList: [T;N];
+
+    for x in 0..N{
+        orderList[x] = Vec::new();
+        /*
+        while(){
+
+        }
+        */
+    }
+}
