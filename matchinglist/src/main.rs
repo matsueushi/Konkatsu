ea9a2553042253201989c71a3d44ae1fbd0bf352
matsueushi <sinxcosxtanx@gmail.com>
2017-11-14 23:01:24 +0900

    Make it possible to change the number to choose

--- matchinglist/src/main.rs
+++ matchinglist/src/main.rs
@@ -3,19 +3,21 @@ use rand::{thread_rng, sample};
 use std::fs;
 use std::io::{BufWriter, Write};
 
-fn main(){
+fn main() {
     let mut rng = thread_rng();
     let n = 26;
-    let m = 3;
+    let m = 5;
     let mut f = BufWriter::new(fs::File::create("machinglist").unwrap());
-    for _ in 1..n{
-        let sample = sample(&mut rng, 1..n, m);
-        let a1 = sample[0].to_string();
-        let a2 = sample[1].to_string();
-        let a3 = sample[2].to_string();
-        let a4 = "\n".to_string();
-        let a_sum = [a1, a2, a3, a4].join(" ");
-        let b = a_sum.as_bytes();
+    for _ in 1..n {
+        let mut sample = sample(&mut rng, 1..n, m).iter().fold(
+            String::new(),
+            |acc, &x| {
+                acc + &*x.to_string() + " "
+            },
+        );
+        sample.pop();
+        sample = sample + "\n";
+        let b = sample.as_bytes();
         f.write(b).unwrap();
     }
 }
