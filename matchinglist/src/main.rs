4dbdb53ea02be02d604743276d1f8772b5ef5279
matsueushi <sinxcosxtanx@gmail.com>
2017-11-15 22:57:09 +0900

    Modify random genarating condition

--- matchinglist/src/main.rs
+++ matchinglist/src/main.rs
@@ -6,15 +6,12 @@ use std::io::{BufWriter, Write};
 fn main() {
     let mut rng = thread_rng();
     let n = 26;
-    let m = 5;
+    let m = 4;
     let mut f = BufWriter::new(fs::File::create("machinglist").unwrap());
-    for _ in 1..n {
-        let mut sample = sample(&mut rng, 1..n, m).iter().fold(
-            String::new(),
-            |acc, &x| {
-                acc + &*x.to_string() + " "
-            },
-        );
+    for me in 1..n {
+        let mut sample = sample(&mut rng, (1..n).filter(|&x| x != me), m)
+            .iter()
+            .fold(String::new(), |acc, &x| acc + &*x.to_string() + " ");
         sample.pop();
         sample = sample + "\n";
         let b = sample.as_bytes();
